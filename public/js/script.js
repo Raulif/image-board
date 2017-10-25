29e131cfcfead0bb726af238e9dcb3a0c720e72f
raulif <raul4cade@gmail.com>
2017-10-25 09:45:47 +0200

    Add file uploader

--- public/js/script.js
+++ public/js/script.js
@@ -7,11 +7,27 @@
 });
     Handlebars.partials = Handlebars.templates
 
-    var MyView = Backbone.View.extend({
+    var ImagesModel = Backbone.Model.extend({
+        initialize: function() {
+            this.fetch()
+        },
+        url: '/home'
+    })
+
+    var ImagesView = Backbone.View.extend({
+        el: '#main',
         initialize: function() {
             var view = this
             this.model.on('change', function(req, res) {
                 view.render();
+                var uploadModel = new UploadModel
+                uploadModel.on('success', function() {
+                    view.model.fetch()
+                })
+                new UploadView({
+                    el: '#uploaddiv',
+                    model: uploadModel
+                })
             })
         },
         render: function() {
@@ -19,22 +35,55 @@
         }
     })
 
-    var MyModel = Backbone.Model.extend({
-        initialize: function() {
-            this.fetch()
+    var UploadModel = Backbone.Model.extend({
+        save: function() {
+            var formData = new FormData();
+            formData.append('imageFile', this.get('file'));
+            var model = this
+            $.ajax({
+                url: '/upload',
+                method: 'POST',
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function() {
+                    model.trigger('success')
+                }
+            })
         },
-        url: '/home'
+        url: '/upload'
+    })
+
+
+    var UploadView = Backbone.View.extend({
+        el: '#main',
+        initialize: function() {
+            var view = this
+            this.render();
+        },
+        render: function() {
+            this.$el.html(Handlebars.templates.uploadset(this.model.toJSON()))
+        },
+        events: {
+            'click #uploadbutton': function(){
+                var file = $('input[type="file"]').get(0).files[0];
+                this.model.set({
+                    file: file
+                }).save()
+            }
+        }
     })
 
     var Router = Backbone.Router.extend({
         routes: {
             'home': 'home',
         },
-        home: function() {
-            new MyView({
-                el: '#main',
-                model: new MyModel
 
+        home: function() {
+            var imagesModel = new ImagesModel
+            new ImagesView({
+                el: '#main',
+                model: imagesModel
             })
         }
     })
--- server.js
+++ server.js
@@ -3,33 +3,69 @@ const app = express()
 const bodyParser = require('body-parser')
 const spicedPg = require('spiced-pg');
 const db = spicedPg(process.env.DATABASE_URL || 'postgres:rauliglesias:Fourcade1@localhost:5432/imageboard');
+const multer = require('multer')
+const uidSafe = require('uid-safe')
+const path = require('path')
+var toS3 = require('./toS3').toS3;
+
+
+var diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + '/uploads');
+    },
+    filename: function (req, file, callback) {
+      uidSafe(24).then(function(uid) {
+          callback(null, uid + path.extname(file.originalname));
+      });
+    }
+});
+
+var uploader = multer({
+    storage: diskStorage,
+    limits: {
+        filesize: 2097152
+    }
+});
+
 
 app.use(express.static(`${__dirname}/public`))
+// app.use(express.static(`${__dirname}/uploads`))
+
 app.use(bodyParser.urlencoded({
     extended: false
 }))
 app.use(bodyParser.json())
 
+app.post('/upload', uploader.single('imageFile'), function(req, res) {
+    // If nothing went wrong the file is already in the uploads directory
+    console.log('filename is ',req.file.filename);
+
+    if(req.file) {
+        toS3(req.file)
+        .then(function(){
+            //only after this, do the insert query to DB
+            const qInsertImage = `INSERT INTO images (image, username, title, description) VALUES ($1, $2, $3, $4)`
+
+            const params = [req.file.filename, "raulif", "test title", "test description"]
+
+            return db.query(qInsertImage, params).then(()=> {
+                res.json({success: true})
+            })
+        })
+        .catch(err => res.json({success: false}));
+    } else {
+        res.json({success: false})
+    }
+});
+
 app.get('/home', (req, res) => {
-    var queryImages = `SELECT image FROM images`;
-    db.query(queryImages).then((results) => {
+    const qShowImages = `SELECT image, title FROM images`;
+    db.query(qShowImages).then((results) => {
         var images = results.rows;
-        var jsonFile = require(__dirname + '/config.json')
-        var urlString = jsonFile.s3Url
-        console.log(urlString);
         res.json({
             images: images,
         })
     }).catch(err => console.log(err));
 })
-
-
-// function setUserName(req, res) {
-//
-// }
-//
-// app.put('/user', setUserName)
-// app.post('/user', setUserName)
-
 
 app.listen(8080, () => {console.log('listening')})
